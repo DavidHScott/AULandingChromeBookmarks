9504cba4a8df7b09cd229d9834c2a19fdfe689c9
David Scott <legoloverdavid@gmail.com>
2019-01-09 16:26:36 -0500

    Add files via upload

---
+++ bookmarklet.js
@@ -0,0 +1,19 @@
+location.href = 'https://landing.athabascau.ca/bookmarks/add/40166?address=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(document.title) + '&bmfolder_guid=95148'
+
+//https://landing.athabascau.ca/groups/profile/2162391/comp268
+//comp282 - 15278 https://landing.athabascau.ca/groups/profile/15278/comp-282
+//https://landing.athabascau.ca/groups/profile/1849734/comp-410
+//https://landing.athabascau.ca/groups/profile/126104/comp-470-landing-group
+//https://landing.athabascau.ca/groups/profile/1164158/comp-607
+//https://landing.athabascau.ca/groups/profile/707/comp-610
+//https://landing.athabascau.ca/groups/profile/720/comp-650-social-computing
+//https://landing.athabascau.ca/groups/profile/1465520/comp495495-computer-and-information-systems-project
+//https://landing.athabascau.ca/groups/profile/189005/comp-444/tab/235868/default
+//https://landing.athabascau.ca/groups/profile/398939/comp-348
+//https://landing.athabascau.ca/groups/profile/38551/comp-470
+//https://landing.athabascau.ca/groups/profile/346818/comp-601
+//https://landing.athabascau.ca/groups/profile/94802/comp-602-enterprise-information-management
+//https://landing.athabascau.ca/groups/profile/1078845/comp-604-networks-project-team
+//https://landing.athabascau.ca/groups/profile/720/comp-650-social-computing
+//https://landing.athabascau.ca/groups/profile/71651/comp361-systems-analysis-and-design
+//https://landing.athabascau.ca/groups/profile/125938/comp372-design-and-analysis-of-algorithms
