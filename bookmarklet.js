a2a56e169436ee70546bfc6e3d5883bf7d009c9d
David Scott <legoloverdavid@gmail.com>
2019-01-12 07:36:29 -0500

    Delete bookmarklet.js

--- bookmarklet.js
+++
@@ -1,1 +0,0 @@
-location.href = 'https://landing.athabascau.ca/bookmarks/add/40166?address=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(document.title) + '&bmfolder_guid=95148'
